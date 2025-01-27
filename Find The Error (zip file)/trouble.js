f61ecbf7f19fb06d19f1ce4d5bfe51bc1c26d565
Masoud Moawiye <mnmoawiye@my.waketech.edu>
2025-01-27 14:02:09 -0500

    commited

--- Find The Error (zip file)/trouble.js
+++ Find The Error (zip file)/trouble.js
@@ -1,7 +1,7 @@
 let v1 = 3;
 let v2 = 4;  // Added semicolon
 
-alert("3 times 4" + equals + v1 * v2);
+alert("3 times 4" + "equal" + v1 * v2);
 
 // This sets the general styling for the divs within the section tag using .style
 let section = document.getElementById("s1");  // Changed getElementsById to getElementById
