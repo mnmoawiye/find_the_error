6d82d06fcb9887de914dc4b2ffcf0de49f7fe96d
Masoud Moawiye <mnmoawiye@my.waketech.edu>
2025-01-28 11:13:17 -0500

    commited

---
+++ trouble.js
@@ -0,0 +1,47 @@
+let v1 = 3;
+let v2 = 4;  // Added semicolon
+
+alert("3 times 4" + "equal" + v1 * v2);
+
+// This sets the general styling for the divs within the section tag using .style
+let section = document.getElementById("s1");  // Changed getElementsById to getElementById
+section.style.width = "50%";
+section.style.textAlign = "center";
+section.style.fontFamily = "arial, serif";
+section.style.fontWeight = "bold";  // changed "style" to "stlye"
+section.style.fontStyle = "italic";
+section.style.fontSize = "20px";
+
+// Sets the customization for each individual color/div tag
+// .innerHTML is used to add content within each div tag.
+// .style.backgroundColor is used to assign a different background color to each div
+
+// RED
+const red = document.getElementById("red");
+red.innerHTML = "RED";
+red.style.backgroundColor = "red";
+
+// ORANGE
+const orange = document.getElementById("orange");  // changed innerhtml to innerHTML
+orange.innerHTML = "ORANGE";
+orange.style.backgroundColor = "orange";
+
+// YELLOW
+const yellow = document.getElementById("yellow");
+yellow.innerHTML = "YELLOW";
+yellow.style.backgroundColor = "yellow";
+
+// GREEN
+const green = document.getElementById("green");  // Changed purple to green
+green.innerHTML = "GREEN";
+green.style.backgroundColor = "green";
+
+// INDIGO
+const indigo = document.getElementById("indigo");
+indigo.innerHTML = "INDIGO";
+indigo.style.backgroundColor = "indigo";
+
+// VIOLET
+const violet = document.getElementById("violet");
+violet.innerHTML = "VIOLET";
+violet.style.backgroundColor = "violet";
